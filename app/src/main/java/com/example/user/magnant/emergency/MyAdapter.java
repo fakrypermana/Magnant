eb028ee713598ffad2451c4d6c60cb5170a33ec1
fakrypermana <fakrypermana@gmail.com>
2018-11-01 00:15:25 +0700

    added card view

--- app/src/main/java/com/example/user/magnant/emergency/MyAdapter.java
+++ app/src/main/java/com/example/user/magnant/emergency/MyAdapter.java
@@ -2,18 +2,21 @@ package com.example.user.magnant.emergency;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.v7.recyclerview.R;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.user.magnant.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     private Context context;
-    private List<ListofEmergencyItem> listE = new ArrayList<>();
+    private List<ListofEmergencyItem> listE;
 
     public MyAdapter(Context context, List<ListofEmergencyItem> listE) {
         this.context = context;
@@ -23,24 +26,32 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View view = LayoutInflater.from(viewGroup.getContext()).
+                inflate(R.layout.item_list_emergency, viewGroup, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-
+        ListofEmergencyItem listItem = listE.get(i);
+        viewHolder.tvHead.setText(listItem.getHead());
+        viewHolder.tvDesc.setText(listItem.getDesc());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listE.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-
+        public TextView tvHead, tvDesc;
+        public CardView cvEmergency;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            tvHead = itemView.findViewById(R.id.textViewHead);
+            tvDesc = itemView.findViewById(R.id.textViewDesc);
+            cvEmergency = itemView.findViewById(R.id.rv_emergency);
         }
     }
 }
