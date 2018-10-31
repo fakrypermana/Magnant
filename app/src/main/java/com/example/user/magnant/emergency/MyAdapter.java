78959c73c2e7235d9bff429901505a92d62c406f
fakrypermana <fakrypermana@gmail.com>
2018-10-31 20:29:23 +0700

    add some .java form adapter

--- app/src/main/java/com/example/user/magnant/emergency/MyAdapter.java
+++ app/src/main/java/com/example/user/magnant/emergency/MyAdapter.java
@@ -1,4 +1,46 @@
 package com.example.user.magnant.emergency;
 
-public class MyAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.recyclerview.R;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private Context context;
+    private List<ListofEmergencyItem> listE = new ArrayList<>();
+
+    public MyAdapter(Context context, List<ListofEmergencyItem> listE) {
+        this.context = context;
+        this.listE = listE;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
