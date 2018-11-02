6cd1fb88d89bd957bb0b987ecb5fb59902aab50e
fakrypermana <fakrypermana@gmail.com>
2018-11-02 23:25:47 +0700

    get number from .getDesc()

--- app/src/main/java/com/example/user/magnant/emergency/EmergencyAdapter.java
+++ app/src/main/java/com/example/user/magnant/emergency/EmergencyAdapter.java
@@ -9,17 +9,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.user.magnant.ClickListener;
 import com.example.user.magnant.R;
 
+import java.lang.ref.WeakReference;
 import java.util.List;
 
 public class EmergencyAdapter extends RecyclerView.Adapter<EmergencyAdapter.ViewHolder> {
-    private Context context;
     private List<EmergencyItem> listE;
+    private WeakReference<ClickListener> listenerWeakRef;
+    private ClickListener listener;
 
-    public EmergencyAdapter(Context context, List<EmergencyItem> listE) {
-        this.context = context;
+    public EmergencyAdapter(List<EmergencyItem> listE, ClickListener listener) {
         this.listE = listE;
+        this.listener = listener;
     }
 
     @NonNull
@@ -27,7 +30,7 @@ public class EmergencyAdapter extends RecyclerView.Adapter<EmergencyAdapter.View
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(viewGroup.getContext()).
                 inflate(R.layout.item_list_emergency, viewGroup, false);
-        return new ViewHolder(view);
+        return new ViewHolder(view, listener);
     }
 
     @Override
@@ -35,6 +38,13 @@ public class EmergencyAdapter extends RecyclerView.Adapter<EmergencyAdapter.View
         EmergencyItem listItem = listE.get(i);
         viewHolder.tvHead.setText(listItem.getHead());
         viewHolder.tvDesc.setText(listItem.getDesc());
+        /**viewHolder.cvEmergency.setTag(i);
+        viewHolder.cvEmergency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                printIt(v);
+            }
+        });**/
 
     }
 
@@ -43,21 +53,28 @@ public class EmergencyAdapter extends RecyclerView.Adapter<EmergencyAdapter.View
         return listE.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public TextView tvHead, tvDesc;
         public CardView cvEmergency;
 
-        public ViewHolder(@NonNull View itemView) {
+        public ViewHolder(@NonNull View itemView, ClickListener clickListener) {
             super(itemView);
+            listenerWeakRef = new WeakReference<>(clickListener);
+
             tvHead = itemView.findViewById(R.id.textViewHead);
             tvDesc = itemView.findViewById(R.id.textViewDesc);
             cvEmergency = itemView.findViewById(R.id.cv_emergence);
-            cvEmergency.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    System.out.println("Clicked info: " + v.getContext());
-                }
-            });
+
+            cvEmergency.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View v) {
+            listenerWeakRef.get().onPositionClicked(getAdapterPosition());
+        }
+    }
+
+    public void printIt(View v){
+        System.out.println("Clicked debug_info:  " + (int)v.getTag());
     }
 }
--- app/src/main/java/com/example/user/magnant/emergency/EmergencyFragment.java
+++ app/src/main/java/com/example/user/magnant/emergency/EmergencyFragment.java
@@ -1,15 +1,23 @@
 package com.example.user.magnant.emergency;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.telecom.Connection;
+import android.telecom.ConnectionRequest;
+import android.telecom.PhoneAccountHandle;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.user.magnant.ClickListener;
 import com.example.user.magnant.R;
 
 import java.util.ArrayList;
@@ -19,6 +27,7 @@ public class EmergencyFragment extends Fragment {
     private Toolbar toolbar;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
+    private static final String TAG = "qcel";
 
     private List<EmergencyItem> listItem;
 
@@ -29,8 +38,9 @@ public class EmergencyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_emergency, container, false);
+        final View view = inflater.inflate(R.layout.fragment_emergency, container, false);
 
         toolbar = view.findViewById(R.id.toolbar_emergency);
         ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
@@ -50,7 +60,18 @@ public class EmergencyFragment extends Fragment {
 
             this.listItem.add(listItem);
         }
-        adapter = new EmergencyAdapter(getActivity(), listItem);
+        adapter = new EmergencyAdapter(listItem, new ClickListener() {
+            @Override
+            public void onPositionClicked(int position) {
+                // Get data for number
+                Log.d(TAG, "onPositionClicked: "+listItem.get(position).getDesc());
+            }
+
+            @Override
+            public void onLongClicked(int position) {
+
+            }
+        });
 
         recyclerView.setAdapter(adapter);
 
